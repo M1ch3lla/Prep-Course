ecb403bac559f0fa79d1568b2d7e3dd8dfb37c77
unknown <you@example.com>
2022-09-17 20:16:20 -0300

    03 v2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -99,7 +99,11 @@ function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (numero < 50 & numero > 20) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function esEntero(numero) {
@@ -110,6 +114,11 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+ if (numero === Math.floor(numero)) {
+  return true;
+ } else {
+  return false;
+ }
 }
 
 function fizzBuzz(numero) {
@@ -117,6 +126,16 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if (numero % 3 === 0) {
+    return "fizz";
+  } else if (numero % 5 === 0) {  
+    return "buzz";
+  } else if (numero % 3 === 0 & numero % 5 === 0) {
+    return "fizzbuzz";
+  } else {
+    return numero;
+  }
+  
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -126,6 +145,18 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if (num1 > num2 & num1 > num3 & num1 > 0) {
+    return "Numero 1 es mayor y positivo";
+  } else if (num1 < 0 || num2 < 0 || num3 < 0) {
+    return "Hay negativos";
+  } else if (num3 > num2) {
+    num3++;
+    return num3;
+  } else if (num1 === 0 ||num2 === 0 || num3 === 0) { 
+    return "Error";
+  } else {
+    return false;
+  }
 }
 
 function esPrimo(numero) {
@@ -134,6 +165,7 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  
 }
 
 function esVerdadero(valor){
